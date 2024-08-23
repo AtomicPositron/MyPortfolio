b8469905af5a4c87fb7493d64c82491bc51c8517
MichaelIbia12 <ietorobong@gmail.com>
2024-08-24 00:38:07 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['*.html', './src/**/*.{js, jsx, ts, tsx, vue}', './src/components/*.vue'],
+  theme: {
+    extend: {
+      animation: {},
+      keyframes: {}
+    }
+  },
+  plugins: []
+}
